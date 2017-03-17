97c11b8e0ff6ff7297b571823960acfbe555b42e
Alexander Pitchford <alex.pitchford@gmail.com>
2017-03-17 07:34:35 +0000

    V4.1 update (#18)
    * Version upgraded to 4.1.0
    Windows now supported on py>=35
    Attempt to make mingw compile conditional on py=27 or py=34
    
    * source files updated - hpp now included
    
    * py2 is not a valid selector
    
    * Change CONDA_PY clauses to string value comparisons
    
    * dist updated again to try and fix OSX compile
    
    * toolchain added, additional py and numpy envs added
    
    * MNT: Re-rendered with conda-smithy 2.1.1
    
    * Fix for OSX sort applied
    
    * MNT: Re-rendered with conda-smithy 2.1.1
    
    * MNT: Re-rendered with conda-smithy 2.1.1
    
    * MNT: Re-rendered with conda-smithy 2.1.1 (win py36 back)
    
    * New source files with handling of cpu_count exception
    
    * skip win py27 build
    Due to error on import scipy.misc

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,21 @@
-"%PYTHON%" setup.py build --compiler=mingw32
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py build_ext --compiler=mingw32
-if errorlevel 1 exit 1
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
+set MING=0
+echo CONDA_PY "%CONDA_PY%"
+if "%CONDA_PY%"=="27" set MING=1
 
+if %MING%==1 (
+    echo using mingw compiler
+    "%PYTHON%" setup.py build --compiler=mingw32
+    if errorlevel 1 exit 1
+    "%PYTHON%" setup.py build_ext --compiler=mingw32
+    if errorlevel 1 exit 1
+    "%PYTHON%" setup.py install
+    if errorlevel 1 exit 1
+) ELSE (
+    echo using default compiler
+    "%PYTHON%" setup.py build
+    if errorlevel 1 exit 1
+    "%PYTHON%" setup.py build_ext
+    if errorlevel 1 exit 1
+    "%PYTHON%" setup.py install
+    if errorlevel 1 exit 1
+)
